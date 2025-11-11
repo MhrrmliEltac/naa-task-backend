52b0a8ddf7c5e4bff3af0640af9114bae7037ed2
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-11-11 22:06:25 +0400

    added db.js

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import serverless from "serverless-http";
-
 import contentRoutes from "./routes/contentRoutes.routes.js";
+import connectDb from "./lib/db.js";
 
 dotenv.config();
-
 const app = express();
+connectDb();
 
 // Middleware-lər
 app.use(cors());
 app.use(express.json());
 
-// MongoDB-yə qoşulma
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
-
 // Routes
 app.use("/api/content", contentRoutes);
+
+app.listen(5000, () => {
+  console.log("server ugurla isledi");
+});
 
 // Vercel üçün export
 export const handler = serverless(app);
