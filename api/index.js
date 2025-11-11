4780645e228ff8eae338fe8dbc55e9fc00dc1c98
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-11-11 22:31:51 +0400

    Update vercel.json to use rewrites for API routing and simplify database connection logic in db.js by removing retry and SSL handling. Enhance error logging for MongoDB connection failures.

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import contentRoutes from "../routes/contentRoutes.routes.js";
+import connectDb from "../lib/db.js";
+
+dotenv.config();
+connectDb();
+
+const app = express();
+
+// Middleware-lər
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/content", contentRoutes);
+
+app.listen(5000, () => {
+  console.log("server ugurla isledi");
+});
+
+// Vercel üçün export
+export default app;
+
