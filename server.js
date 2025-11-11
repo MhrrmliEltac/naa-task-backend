7f3f6ef31a3a3cdc0d69c2e74347aaf94fb46c7e
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-11-11 21:40:36 +0400

    Vercel folder added

--- server.js
+++ server.js
@@ -24,3 +24,5 @@ app.use("/api/content", contentRoutes);
 // Serveri işə sal
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+ 
+export default app
